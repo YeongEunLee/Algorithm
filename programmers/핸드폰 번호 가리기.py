b178dc23c41edc2393d2cdcaad104680ce28cda4
YeongEunLee <zduddmszz@gmail.com>
2021-02-05 19:34:53 +0900

    핸드폰 번호 가리기

--- programmers/핸드폰 번호 가리기.py
+++ programmers/핸드폰 번호 가리기.py
@@ -1,2 +1,7 @@
 def solution(phone_number):
     return phone_number.replace(phone_number[:-4],'*'*(len(phone_number)-4))
+
+# 문자열.replace("바꿔야하는 문자", "바꿀문자")
+#len(문자열길이)-4 -> 4개 빼고 바꾸야하므로
+#문자열[:-4] -> 뒤에 4개
+# "x"*(개수) -> 개수만큼 문자열 반복
