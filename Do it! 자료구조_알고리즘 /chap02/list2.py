ccf0c4c9d34626fa6f2baa84944c46bc4addf289
YeongEunLee <zduddmszz@gmail.com>
2021-02-18 16:11:41 +0900

    chap01

---
+++ Do it! 자료구조_알고리즘 /chap02/list2.py
@@ -0,0 +1,4 @@
+x = ['John', 'George', 'Paul', 'Ringo']
+
+for i, name in enumerate(x): #eumerate()함수로 모든 원소 스캔
+  print(f'x[{i}] = {name}')
---
+++ Do it! 자료구조_알고리즘 /chap02/list3.py
@@ -0,0 +1,4 @@
+x = ['John', 'George', 'Paul', 'Ringo']
+
+for i, name in enumerate(x, 1): #eumerate()함수로 모든 원소 스캔, 1부터 카운트 !
+  print(f'{i}번째 = {name}')
