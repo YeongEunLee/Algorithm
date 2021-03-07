f3ee36fd78fae7ffe2fce0499a6d38a9beea1264
YeongEunLee <zduddmszz@gmail.com>
2021-03-07 17:12:56 +0900

    퀵 정렬

--- Ch6_정렬 알고리즘/퀵정렬_1.py
+++ Ch6_정렬 알고리즘/퀵정렬_1.py
@@ -6,8 +6,7 @@ def quick_sort(array):
   pivot = array[0] # 피벗은 첫 번째 원소
   tail = array[:1] # 피벗을 제외한 리스트
   
-	left_side = [x for x in tail if x <= pivot] #분할된 왼쪽 부분
-	right_side = [x for x in tail if x > pivot] #분할된 오른쪽 부분
-
+	left_side = [x for x in tail if x <= pivot] # 분할된 왼쪽
+	right_side = [x for x in tail if x > pivot] # 분할된 오른쪽 부분
 	return quick_sort(left_side) + [pivot] + quick_sort(right_side)
 print(quick_sort(array))
